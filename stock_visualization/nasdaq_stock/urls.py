0b0c8152e841a876d0bc655fc9ed1500b36e42cf
Xuanfu Chen <41319054+xuanfuchen@users.noreply.github.com>
2022-05-20 10:47:47 -0400

    add first world chart, not working
    -add world.json in static file for world chart
    -add JS code in test.html for testing
    -failed to render the chart for some reason

--- stock_visualization/nasdaq_stock/urls.py
+++ stock_visualization/nasdaq_stock/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from nasdaq_stock import views
 
 urlpatterns = [
-    path('macro/', views.hello),
+    path('macro/', views.macro),
 ]
--- stock_visualization/nasdaq_stock/views.py
+++ stock_visualization/nasdaq_stock/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def hello(request):
     return render(request, 'html/test.html')
+
+def macro(request):
+    data = []
+    return render(request, 'html/test.html')
