8a11d3086c2e5889b5b6952bd9d2eb34b603720f
Xuanfu Chen <41319054+xuanfuchen@users.noreply.github.com>
2022-05-20 21:54:59 -0400

    top 10 bar chart done
    -top ten bar chart shows top 10 companies based on sales

--- stock_visualization/nasdaq_stock/views.py
+++ stock_visualization/nasdaq_stock/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from nasdaq_stock.models import CompanyInfo
+import operator
+from django.db.models import IntegerField
+from django.db.models.functions import Cast
 
 # Create your views here.
 def main(request):
-    return render(request, 'contents/main.html')
+    companies = CompanyInfo.objects.annotate(int_sales = Cast('sales', IntegerField())).order_by('-int_sales')[:10]
+    symbol = []
+    sales = []
+    employees = []
+    for company in companies:
+        symbol.append(company.stock_symbol)
+        sales.append(company.sales)
+        employees.append(company.employees)
+    dbData = { 
+        "symbol": symbol,
+        "sales": sales, 
+        "employees": employees
+    }
+    return render(request, 'contents/main.html', dbData)
 
 def macroMap(request):
     data = []
