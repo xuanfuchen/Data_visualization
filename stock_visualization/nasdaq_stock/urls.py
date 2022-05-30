ea9674cdb57273c987ae2ceb2fc0c32769f8cb9f
Xuanfu Chen <41319054+xuanfuchen@users.noreply.github.com>
2022-05-29 21:17:42 -0400

    changes on the main page (not done yet)
    -change the name from main page to home page
    
    -world maps are now putted on the home page, also, other charts are changed to show sector info from showing top 10 comp info
    
    -views.py is changed to match the data of the new home page need

--- stock_visualization/nasdaq_stock/urls.py
+++ stock_visualization/nasdaq_stock/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from nasdaq_stock import views
 
 urlpatterns = [
-    path('main/', views.main, name = 'main'),
+    path('home/', views.home, name = 'home'),
     path('maps/', views.macroMap, name = 'maps'),
     path('company_list/', views.companyList, name = 'companyList'),
     path('company_list/<str:symbol>/', views.companyDetail, name='companyDetail'),
--- stock_visualization/nasdaq_stock/views.py
+++ stock_visualization/nasdaq_stock/views.py
@@ -10,19 +10,21 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 
 # Create your views here.
-def main(request):
+def home(request):
     # ========================================================== data for bar chart and pie chart =============================================
-    topCompanies = CompanyInfo.objects.annotate(int_sales = Cast('sales', IntegerField())).order_by('-int_sales')[:10]
-    compNameList = []
-    symbolList = []
-    salesList = []
-    employees = []
-    for company in topCompanies:
-        compNameList.append(company.company_name)
-        symbolList.append(company.stock_symbol)
-        salesList.append(company.sales)
-        employees.append(company.employees)
+    # topCompanies = CompanyInfo.objects.annotate(int_sales = Cast('sales', IntegerField())).order_by('-int_sales')[:10]
+    # compNameList = []
+    # symbolList = []
+    # salesList = []
+    # employees = []
+    # for company in topCompanies:
+    #     compNameList.append(company.company_name)
+    #     symbolList.append(company.stock_symbol)
+    #     salesList.append(company.sales)
+    #     employees.append(company.employees)
     
+    top_sector_name_list = []
+    top_sector_sales_list = []
     allComp = CompanyInfo.objects.all()
     sector_total_dict = defaultdict(int)
     sector_total_list = []
@@ -36,7 +38,8 @@ def main(request):
     for x in top_dict:
         dict = {'value': x[1], 'name': x[0]}
         sector_total_list.append(dict)
-    
+        top_sector_name_list.append(x[0])
+        top_sector_sales_list.append(x[1])
 
     # ========================================================== data for line chart ==========================================================
     # need a list [
@@ -47,54 +50,100 @@ def main(request):
     # ]
 
 
-    # first get all dates that top 10 companies have record
-    dateList = []
-    for topCompany in topCompanies:
-        symbol = topCompany.stock_symbol
-        topPriceHistory = PriceHistory.objects.filter(stock_symbol = symbol)
-        for dayPrice in topPriceHistory:
-            if dayPrice.price_date not in dateList:
-                dateList.append(dayPrice.price_date)
-    dateList = sorted(dateList)
+    # # first get all dates that top 10 companies have record
+    # dateList = []
+    # for topCompany in topCompanies:
+    #     symbol = topCompany.stock_symbol
+    #     topPriceHistory = PriceHistory.objects.filter(stock_symbol = symbol)
+    #     for dayPrice in topPriceHistory:
+    #         if dayPrice.price_date not in dateList:
+    #             dateList.append(dayPrice.price_date)
+    # dateList = sorted(dateList)
 
-    # create the first list in the return list
-    dates = ['dates']
-    for date in dateList:
-        dates.append(date.strftime("%b %d"))
+    # # create the first list in the return list
+    # dates = ['dates']
+    # for date in dateList:
+    #     dates.append(date.strftime("%b %d"))
     
-    # create the list that will be sent to the front-end
-    lineChartData = []
-    lineChartData.append(dates)
+    # # create the list that will be sent to the front-end
+    # lineChartData = []
+    # lineChartData.append(dates)
 
-    for topCompany in topCompanies:
-        # iterate the top company list, topCompany belongs to CompanyInfo.objects
-        companyPriceList = []
-        symbol = topCompany.stock_symbol
-        companyPriceList.append(symbol)
-        # iterate through every possible date
-        for date in dateList:
-            # try to find corrispond price to the date, and add it to the list for that company
-            try:
-                datePrice = PriceHistory.objects.get(stock_symbol = symbol, price_date=date)
-                price = datePrice.close
-                companyPriceList.append(float(price))
-            # if the data doesn't exist, put a null there as a placeholder
-            except:
-                companyPriceList.append(NULL)
+    # for topCompany in topCompanies:
+    #     # iterate the top company list, topCompany belongs to CompanyInfo.objects
+    #     companyPriceList = []
+    #     symbol = topCompany.stock_symbol
+    #     companyPriceList.append(symbol)
+    #     # iterate through every possible date
+    #     for date in dateList:
+    #         # try to find corrispond price to the date, and add it to the list for that company
+    #         try:
+    #             datePrice = PriceHistory.objects.get(stock_symbol = symbol, price_date=date)
+    #             price = datePrice.close
+    #             companyPriceList.append(float(price))
+    #         # if the data doesn't exist, put a null there as a placeholder
+    #         except:
+    #             companyPriceList.append(NULL)
         
-        # add the list to the return list
-        lineChartData.append(companyPriceList)
+    #     # add the list to the return list
+    #     lineChartData.append(companyPriceList)
 
+    
+    # ========================================================== data for maps ==========================================================
+    count_dict = defaultdict(int)
+    sales_dict = defaultdict(int)
+    companies = CompanyInfo.objects.all()
+    count_list = []
+    sales_list = []
+
+    country_count = []
+    comp_count = []
+    country_sales = []
+    comp_sales = []
+    for company in companies:
+        if company.country == "-":
+            # count_dict["Unknown"] += 1
+            pass
+        else:
+            count_dict[company.country] += 1
+            sales_dict[company.country] += int(company.sales)
+
+    
+    sorted_count = sorted(count_dict.items(), key = lambda item: item[1], reverse=True)
+    for x in sorted_count:
+        dict = {'name': x[0], 'value': x[1]}
+        count_list.append(dict)
+    
+    for x in sorted_count[:20]:
+        country_count.append(x[0])
+        comp_count.append(x[1])
+    
+    sorted_sales = sorted(sales_dict.items(), key = lambda item: item[1], reverse=True)
+    for x in sorted_sales:
+        dict = {'name': x[0], 'value': x[1]}
+        sales_list.append(dict)
+
+    for x in sorted_sales[:20]:
+        country_sales.append(x[0])
+        comp_sales.append(x[1])
     # ========================================================== sent data to the front-end ===================================================
     dbData = { 
-        "compName": compNameList,
-        "comp_symbol": symbolList,
-        "sales": salesList, 
-        "employees": employees,
+        # "compName": compNameList,
+        # "comp_symbol": symbolList,
+        # "sales": salesList, 
+        # "employees": employees,
+        # "lineChartData": lineChartData,
+        "top_sector_name_list": top_sector_name_list,
+        "top_sector_sales_list": top_sector_sales_list,
         "sector_total_list": sector_total_list,
-        "lineChartData": lineChartData,
+        "count_list": count_list,
+        "sales_list": sales_list,
+        "country_count": country_count,
+        "comp_count": comp_count,
+        "country_sales": country_sales,
+        "comp_sales": comp_sales,
     }
-    return render(request, 'contents/main.html', dbData)
+    return render(request, 'contents/home.html', dbData)
 
 
 
