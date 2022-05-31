16a839fc27ef84c2ff2bfff21f2b1636684bb01a
Xuanfu Chen <41319054+xuanfuchen@users.noreply.github.com>
2022-05-30 21:48:48 -0400

    home page modify done
    -add 4 more charts to the home page
    
    -delete global_map page, add new page: Top Companies, for showing top 10 companies by sales
    
    -add two old charts and two new charts to the new page Top Companies

--- stock_visualization/nasdaq_stock/urls.py
+++ stock_visualization/nasdaq_stock/urls.py
@@ -3,7 +3,7 @@ from nasdaq_stock import views
 
 urlpatterns = [
     path('home/', views.home, name = 'home'),
-    path('maps/', views.macroMap, name = 'maps'),
+    path('top_companies/', views.topCompanies, name = 'top_companies'),
     path('company_list/', views.companyList, name = 'companyList'),
     path('company_list/<str:symbol>/', views.companyDetail, name='companyDetail'),
 ]
--- stock_visualization/nasdaq_stock/views.py
+++ stock_visualization/nasdaq_stock/views.py
@@ -11,37 +11,157 @@ from django.db.models import Q
 
 # Create your views here.
 def home(request):
-    # ========================================================== data for bar chart and pie chart =============================================
-    # topCompanies = CompanyInfo.objects.annotate(int_sales = Cast('sales', IntegerField())).order_by('-int_sales')[:10]
-    # compNameList = []
-    # symbolList = []
-    # salesList = []
-    # employees = []
-    # for company in topCompanies:
-    #     compNameList.append(company.company_name)
-    #     symbolList.append(company.stock_symbol)
-    #     salesList.append(company.sales)
-    #     employees.append(company.employees)
-    
-    top_sector_name_list = []
-    top_sector_sales_list = []
+    # ========================================================== get data for top 10 sectors by sales =============================================
+    top_sales_sector_name_list = []
+    top_sales_sector_value_list = []
     allComp = CompanyInfo.objects.all()
-    sector_total_dict = defaultdict(int)
-    sector_total_list = []
+    sector_sales_total_dict = defaultdict(int)
+    sector_sales_total_list = []
     for company in allComp:
         if(company.sector == "-"):
-            sector_total_dict["Undefined"] += int(company.sales)
+            sector_sales_total_dict["Undefined"] += int(company.sales)
         else:
-            sector_total_dict[company.sector] += int(company.sales)
+            sector_sales_total_dict[company.sector] += int(company.sales)
 
-    top_dict = sorted(sector_total_dict.items(), key = lambda item: item[1], reverse=True)[:10]
+    top_dict = sorted(sector_sales_total_dict.items(), key = lambda item: item[1], reverse=True)[:10]
     for x in top_dict:
         dict = {'value': x[1], 'name': x[0]}
-        sector_total_list.append(dict)
-        top_sector_name_list.append(x[0])
-        top_sector_sales_list.append(x[1])
+        sector_sales_total_list.append(dict)
+        top_sales_sector_name_list.append(x[0])
+        top_sales_sector_value_list.append(x[1])
+    
+    # ========================================================== get data for maps ==========================================================
+    count_dict = defaultdict(int)
+    sales_dict = defaultdict(int)
+    companies = CompanyInfo.objects.all()
+    count_list = []
+    sales_list = []
 
-    # ========================================================== data for line chart ==========================================================
+    country_count = []
+    comp_count = []
+    country_sales = []
+    comp_sales = []
+    for company in companies:
+        if company.country == "-":
+            # count_dict["Unknown"] += 1
+            pass
+        else:
+            count_dict[company.country] += 1
+            sales_dict[company.country] += int(company.sales)
+
+    
+    sorted_count = sorted(count_dict.items(), key = lambda item: item[1], reverse=True)
+    for x in sorted_count:
+        dict = {'name': x[0], 'value': x[1]}
+        count_list.append(dict)
+    
+    for x in sorted_count[:20]:
+        country_count.append(x[0])
+        comp_count.append(x[1])
+    
+    sorted_sales = sorted(sales_dict.items(), key = lambda item: item[1], reverse=True)
+    for x in sorted_sales:
+        dict = {'name': x[0], 'value': x[1]}
+        sales_list.append(dict)
+
+    for x in sorted_sales[:20]:
+        country_sales.append(x[0])
+        comp_sales.append(x[1])
+
+    #========================================================== get data for top 10 sectors by total employees ==========================================================
+    top_employees_sector_name_list = []
+    top_employees_sector_value_list = []
+    sector_employees_total_list = []
+
+    sector_employees_total_dict = defaultdict(int)
+    for company in allComp:
+        if(company.sector == "-" or company.sector == "Undefined"):
+            pass
+        else:
+            sector_employees_total_dict[company.sector] += int(company.employees)
+
+    employees_top_dict = sorted(sector_employees_total_dict.items(), key = lambda item: item[1], reverse=True)[:10]
+    for x in employees_top_dict:
+        dict = {'value': x[1], 'name': x[0]}
+        sector_employees_total_list.append(dict)
+        top_employees_sector_name_list.append(x[0])
+        top_employees_sector_value_list.append(x[1])
+    
+    #========================================================== get data for top 10 sectors by numbers ==========================================================
+    top_number_sector_name_list = []
+    top_number_sector_value_list = []
+    sector_bumber_total_list = []
+
+    sector_number_total_dict = defaultdict(int)
+    for company in allComp:
+        if(company.sector == "-" or company.sector == "Undefined"):
+            pass
+        else:
+            sector_number_total_dict[company.sector] += 1
+
+    number_top_dict = sorted(sector_number_total_dict.items(), key = lambda item: item[1], reverse=True)[:10]
+    for x in number_top_dict:
+        dict = {'value': x[1], 'name': x[0]}
+        sector_bumber_total_list.append(dict)
+        top_number_sector_name_list.append(x[0])
+        top_number_sector_value_list.append(x[1])
+
+    # ========================================================== sent data to the front-end ===================================================
+    dbData = { 
+        #send data for top 10 sectors by sales
+        "top_sales_sector_name_list": top_sales_sector_name_list,
+        "top_sales_sector_value_list": top_sales_sector_value_list,
+        "sector_sales_total_list": sector_sales_total_list,
+
+        #send data for maps
+        "count_list": count_list,
+        "sales_list": sales_list,
+        "country_count": country_count,
+        "comp_count": comp_count,
+        "country_sales": country_sales,
+        "comp_sales": comp_sales,
+
+        #send data for top 10 sectors by total employees
+        "top_employees_sector_name_list": top_employees_sector_name_list,
+        "top_employees_sector_value_list": top_employees_sector_value_list,
+        "sector_employees_total_list": sector_employees_total_list,
+        
+        #send data for top 10 sectors by numbers
+        "top_number_sector_name_list": top_number_sector_name_list,
+        "top_number_sector_value_list": top_number_sector_value_list,
+        "sector_bumber_total_list": sector_bumber_total_list,
+    }
+    return render(request, 'contents/home.html', dbData)
+
+
+
+def topCompanies(request):
+    #========================================================== get top 10 companies by sales ==========================================================
+    topCompanies = CompanyInfo.objects.annotate(int_sales = Cast('sales', IntegerField())).order_by('-int_sales')[:10]
+    compNameList = []
+    symbolList = []
+    salesList = []
+    employees = []
+    industry = []
+    country_dict = defaultdict(int)
+    top_ten_sales_pie_data = [] #[{value: sales, name: company}, {value: sales, name: company} ...]
+    for company in topCompanies:
+        compNameList.append(company.company_name)
+        symbolList.append(company.stock_symbol)
+        salesList.append(company.sales)
+        employees.append(company.employees)
+        industry.append(company.industry)
+        country_dict[company.country] += 1
+        top_ten_sales_pie_data.append({'value': company.sales, 'name': company.company_name})
+    
+    country_dict = sorted(country_dict.items(), key = lambda x: x[1], reverse=True)
+    country_name = []
+    country_count = []
+    for x in country_dict:
+        country_name.append(x[0])
+        country_count.append(x[1])
+    
+    # ========================================================== get data for sales line chart ==========================================================
     # need a list [
     # ['dates', date, date, date ... ...]
     # ['comp_1', price, price, price ...]
@@ -50,150 +170,57 @@ def home(request):
     # ]
 
 
-    # # first get all dates that top 10 companies have record
-    # dateList = []
-    # for topCompany in topCompanies:
-    #     symbol = topCompany.stock_symbol
-    #     topPriceHistory = PriceHistory.objects.filter(stock_symbol = symbol)
-    #     for dayPrice in topPriceHistory:
-    #         if dayPrice.price_date not in dateList:
-    #             dateList.append(dayPrice.price_date)
-    # dateList = sorted(dateList)
+    # first get all dates that top 10 companies have record
+    salesDateList = []
+    for topCompany in topCompanies:
+        symbol = topCompany.stock_symbol
+        topPriceHistory = PriceHistory.objects.filter(stock_symbol = symbol)
+        for dayPrice in topPriceHistory:
+            if dayPrice.price_date not in salesDateList:
+                salesDateList.append(dayPrice.price_date)
+    salesDateList = sorted(salesDateList)
 
-    # # create the first list in the return list
-    # dates = ['dates']
-    # for date in dateList:
-    #     dates.append(date.strftime("%b %d"))
+    # create the first list in the return list
+    salesDates = ['dates']
+    for date in salesDateList:
+        salesDates.append(date.strftime("%b %d"))
     
-    # # create the list that will be sent to the front-end
-    # lineChartData = []
-    # lineChartData.append(dates)
+    # create the list that will be sent to the front-end
+    salesLineChartData = []
+    salesLineChartData.append(salesDates)
 
-    # for topCompany in topCompanies:
-    #     # iterate the top company list, topCompany belongs to CompanyInfo.objects
-    #     companyPriceList = []
-    #     symbol = topCompany.stock_symbol
-    #     companyPriceList.append(symbol)
-    #     # iterate through every possible date
-    #     for date in dateList:
-    #         # try to find corrispond price to the date, and add it to the list for that company
-    #         try:
-    #             datePrice = PriceHistory.objects.get(stock_symbol = symbol, price_date=date)
-    #             price = datePrice.close
-    #             companyPriceList.append(float(price))
-    #         # if the data doesn't exist, put a null there as a placeholder
-    #         except:
-    #             companyPriceList.append(NULL)
+    for topCompany in topCompanies:
+        # iterate the top company list, topCompany belongs to CompanyInfo.objects
+        companyPriceList = []
+        symbol = topCompany.stock_symbol
+        companyPriceList.append(symbol)
+        # iterate through every possible date
+        for date in salesDateList:
+            # try to find corrispond price to the date, and add it to the list for that company
+            try:
+                datePrice = PriceHistory.objects.get(stock_symbol = symbol, price_date=date)
+                price = datePrice.close
+                companyPriceList.append(float(price))
+            # if the data doesn't exist, put a null there as a placeholder
+            except:
+                companyPriceList.append(NULL)
         
-    #     # add the list to the return list
-    #     lineChartData.append(companyPriceList)
+        # add the list to the return list
+        salesLineChartData.append(companyPriceList)
 
-    
-    # ========================================================== data for maps ==========================================================
-    count_dict = defaultdict(int)
-    sales_dict = defaultdict(int)
-    companies = CompanyInfo.objects.all()
-    count_list = []
-    sales_list = []
-
-    country_count = []
-    comp_count = []
-    country_sales = []
-    comp_sales = []
-    for company in companies:
-        if company.country == "-":
-            # count_dict["Unknown"] += 1
-            pass
-        else:
-            count_dict[company.country] += 1
-            sales_dict[company.country] += int(company.sales)
-
-    
-    sorted_count = sorted(count_dict.items(), key = lambda item: item[1], reverse=True)
-    for x in sorted_count:
-        dict = {'name': x[0], 'value': x[1]}
-        count_list.append(dict)
-    
-    for x in sorted_count[:20]:
-        country_count.append(x[0])
-        comp_count.append(x[1])
-    
-    sorted_sales = sorted(sales_dict.items(), key = lambda item: item[1], reverse=True)
-    for x in sorted_sales:
-        dict = {'name': x[0], 'value': x[1]}
-        sales_list.append(dict)
-
-    for x in sorted_sales[:20]:
-        country_sales.append(x[0])
-        comp_sales.append(x[1])
-    # ========================================================== sent data to the front-end ===================================================
-    dbData = { 
-        # "compName": compNameList,
-        # "comp_symbol": symbolList,
-        # "sales": salesList, 
-        # "employees": employees,
-        # "lineChartData": lineChartData,
-        "top_sector_name_list": top_sector_name_list,
-        "top_sector_sales_list": top_sector_sales_list,
-        "sector_total_list": sector_total_list,
-        "count_list": count_list,
-        "sales_list": sales_list,
-        "country_count": country_count,
-        "comp_count": comp_count,
-        "country_sales": country_sales,
-        "comp_sales": comp_sales,
-    }
-    return render(request, 'contents/home.html', dbData)
-
-
-
-def macroMap(request):
-    count_dict = defaultdict(int)
-    sales_dict = defaultdict(int)
-    companies = CompanyInfo.objects.all()
-    count_list = []
-    sales_list = []
-
-    country_count = []
-    comp_count = []
-    country_sales = []
-    comp_sales = []
-    for company in companies:
-        if company.country == "-":
-            # count_dict["Unknown"] += 1
-            pass
-        else:
-            count_dict[company.country] += 1
-            sales_dict[company.country] += int(company.sales)
-
-    
-    sorted_count = sorted(count_dict.items(), key = lambda item: item[1], reverse=True)
-    for x in sorted_count:
-        dict = {'name': x[0], 'value': x[1]}
-        count_list.append(dict)
-    
-    for x in sorted_count[:20]:
-        country_count.append(x[0])
-        comp_count.append(x[1])
-    
-    sorted_sales = sorted(sales_dict.items(), key = lambda item: item[1], reverse=True)
-    for x in sorted_sales:
-        dict = {'name': x[0], 'value': x[1]}
-        sales_list.append(dict)
-
-    for x in sorted_sales[:20]:
-        country_sales.append(x[0])
-        comp_sales.append(x[1])
 
     dbData = {
-        "count_list": count_list,
-        "sales_list": sales_list,
+        "compName": compNameList,
+        "comp_symbol": symbolList,
+        "sales": salesList, 
+        "employees": employees,
+        "salesLineChartData": salesLineChartData,
+        "country_name": country_name,
         "country_count": country_count,
-        "comp_count": comp_count,
-        "country_sales": country_sales,
-        "comp_sales": comp_sales,
+        "top_ten_sales_pie_data": top_ten_sales_pie_data,
+
     }
-    return render(request, 'contents/maps.html', dbData)
+    return render(request, 'contents/topCompanies.html', dbData)
 
 
 def companyList(request):
