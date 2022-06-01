63e7bc27e773fc5913b558d0db205636730475fa
Xuanfu Chen <41319054+xuanfuchen@users.noreply.github.com>
2022-06-01 12:46:33 -0400

    changed setting.py for the deployment on AWS

--- stock_visualization/stock_visualization/settings.py
+++ stock_visualization/stock_visualization/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cj^s0#qdafz^d-qn5lghrt5e2v)x)$s-$=)xmh+-rbtj%$a-n8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,8 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
